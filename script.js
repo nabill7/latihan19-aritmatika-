7f88d22f6013ca6ddec98f2f0c425b556b42ee81
bila <zaskia salsabilazaskia68@gmail.com>
2025-11-04 11:09:57 +0700

    membuat kalkulator harga kopi

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ function hitung() {
 let subtotal = harga * jumlah 
 
 //langkah 2: menghitung diskon
+let potongan = subtotal * (diskon / 100)
 // langlah 3: menghitung pajak
+let pajak = subtotal * (11/100)
+
 // lanhkah 4: hitung total
+let total = subtotal - potongan + pajak
 
 // langkah 5: menampilkan hasil
 document.getElementById("hasil").innerHTML = `
-Subtotal: Rp ${subtotal} <br>
+Subtotal: Rp (${subtotal}%): -Rp ${potongan} <br>
+Diskon (${diskon}%): -Rp ${potongan} <br>
+Pajak (11%) : +Rp ${pajak} <br>
+Total bayar : Rp ${total}
 `
 }
